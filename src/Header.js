de72d4d0c99007bbd138f16c9be932dfa9483286
nbaldzhiev <nenko.baldzhiev@gmail.com>
2022-04-11 00:40:26 +0300

    initial header changes

--- src/Header.js
+++ src/Header.js
@@ -11,6 +11,9 @@ function Header() {
           <Link to="/" className="logo-link">
             <img src={ApLogo} placeholder="AP logo" className="logo-image" width="64" height="64"/>
           </Link>
+          <p className="logo-site-title">
+            AP News
+          </p>
         </div>
         <div className="categories-container">
         </div>
